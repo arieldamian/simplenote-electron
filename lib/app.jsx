afdc9854b48b5b1796661a3658451797b18a15ee
Dennis Snell <dennis.snell@automattic.com>
2016-11-03 12:05:08 +0000

    Highlight search result matches
    Resolves #52
    
    Previously when searching through the notes there was no indication
    where inside a note the searched-for text could be found.
    
    This patch introduces a new decorator that will match the searched
    filter and highlight those results by means of an added CSS class.

--- lib/app.jsx
+++ lib/app.jsx
@@ -551,6 +551,7 @@ export const App = connect( mapStateToProps, mapDispatchToProps )( React.createC
 							</div>
 							<NoteEditor
 								editorMode={state.editorMode}
+								filter={state.filter}
 								note={selectedNote}
 								revisions={state.revisions}
 								onSetEditorMode={this.onSetEditorMode}
